07f885e547b4bb62c5b6f00e0a87d194f6aae560
Axdecces <axdecces@gmail.com>
2017-02-11 22:58:26 +0100

    no longer needed to type in 0

--- Body.py
+++ Body.py
@@ -7,16 +7,51 @@ class Body:
         pass
 
     def pyramid(self):
-        print 'Type in the two variables you already know. If you don\'t know a variable, type in "0".'
-        self.a = float(raw_input('a = '))
-        self.h = float(raw_input('h = '))
-        self.M = float(raw_input('M = '))
-        self.O = float(raw_input('O = '))
-        self.V = float(raw_input('V = '))
-        self.hD = float(raw_input('hD = '))
-        self.d = float(raw_input('d = '))
-        self.G = float(raw_input('G = '))
-        self.s = float(raw_input('s = '))
+        print 'Type in the two variables you already know.'
+        try:
+            self.a = float(raw_input('a = '))
+        except(ValueError):
+            self.a = 0
+
+        try:
+            self.h = float(raw_input('h = '))
+        except(ValueError):
+            self.h = 0
+
+        try:
+            self.M = float(raw_input('M = '))
+        except(ValueError):
+            self.M = 0
+
+        try:
+            self.O = float(raw_input('O = '))
+        except(ValueError):
+            self.O = 0
+
+        try:
+            self.V = float(raw_input('V = '))
+        except(ValueError):
+            self.V = 0
+
+        try:
+            self.hD = float(raw_input('hD = '))
+        except(ValueError):
+            self.hD = 0
+
+        try:
+            self.d = float(raw_input('d = '))
+        except(ValueError):
+            self.d = 0
+
+        try:
+            self.G = float(raw_input('G = '))
+        except(ValueError):
+            self.G = 0
+
+        try:
+            self.s = float(raw_input('s = '))
+        except(ValueError):
+            self.s = 0
 
         variables = {}
         if self.a != 0:
@@ -41,12 +76,31 @@ class Body:
         return Pyramid(variables)
 
     def sphere(self):
-        print 'Type in the variable you already know. If you don\'t know a variable, type in "0".'
-        self.r = float(raw_input('r = '))
-        self.d = float(raw_input('d = '))
-        self.V = float(raw_input('V = '))
-        self.M = float(raw_input('O = '))
-        self.U = float(raw_input('U = '))
+        print 'Type in the variable you already know.'
+        try:
+            self.r = float(raw_input('r = '))
+        except(ValueError):
+            self.r = 0
+
+        try:
+            self.d = float(raw_input('d = '))
+        except(ValueError):
+            self.d = 0
+
+        try:
+            self.V = float(raw_input('V = '))
+        except(ValueError):
+            self.V = 0
+
+        try:
+            self.M = float(raw_input('O = '))
+        except(ValueError):
+            self.M = 0
+
+        try:
+            self.U = float(raw_input('U = '))
+        except(ValueError):
+            self.U = 0
 
         variables = {}
         if self.r != 0:
